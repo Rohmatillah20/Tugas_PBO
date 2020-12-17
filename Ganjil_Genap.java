34bddcf4ca22b795ead721b3a8df29dee53ee067
Rohmatillah20 <76101825+Rohmatillah20@users.noreply.github.com>
2020-12-17 19:15:13 +0700

    Add files via upload

---
+++ Ganjil_Genap.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Ganjil_Genap {
+    public static void main(String[] args) {
+        int a;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Masukkan sebuah bilangan : ");
+        a = scan.nextInt();
+        if (a % 2 == 0) {
+            System.out.print("Bilangan " + a + " adalah genap.\n");
+        } else {
+            System.out.print("Bilangan " + a + " adalah ganjil.\n");
+        }
+    }
+}
